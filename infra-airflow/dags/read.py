3b399e5dfb1912a073a67a42ac5100bde3c85e7f
Andressa Evelyn <124847116+Andressa-Evelyn@users.noreply.github.com>
2025-12-27 12:15:47 -0300

    Delete infra-airflow/dags/read.py

--- infra-airflow/dags/read.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_parquet("/home/andressa/fraudflow-spark-pipeline/dados/staging/transactions_fraud_staging/part-00000-6328524b-94a5-4508-9c1a-96a5c0338005-c000.snappy.parquet")
-print(df.head())
